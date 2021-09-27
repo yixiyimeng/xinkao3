9aa95d475be8a2f39ec74b3bd29cecff2586d5ce
zhangjing <zhangjing@huixiangtech.cn>
2021-09-27 16:25:07 +0800

    12

--- src/renderer/page/mainPage/api/index.js
+++ src/renderer/page/mainPage/api/index.js
@@ -65,6 +65,7 @@ const api = {
 	saveQuestionImgAndSend:'/common/saveQuestionImgAndSend',//保存题干
 	importTestQuestions:'/platform/importTestQuestions',//上传家庭作业试卷
 	getAnswerMsg:'answerMsg/getAnswerMsg',//随堂检测统计
+	// yu
 }
 export default api
 export function postAction(url, parameter, obj = {}) {
